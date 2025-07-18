8115d6ac6a007062fc3b740d11c1f0de223a0cf1
Martin Bonnin <martin@mbonnin.net>
2025-07-18 17:11:24 +0200

    Update CI workflows to work with new Maven Central secrets (#6)
    * Update CI workflows to work with new Maven Central secrets
    
    * bump nmcp

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.gradleup.librarian.gradle.Librarian
 plugins {
   alias(libs.plugins.kgp).apply(false)
   alias(libs.plugins.librarian).apply(false)
+  alias(libs.plugins.nmcp).apply(false)
 }
 
 Librarian.root(project)
